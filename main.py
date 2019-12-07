c6eec53bfb0ae177cc6ba4120186ef099578ea27
MinsukKim <minsuk.kim.001@student.uni.lu>
2019-12-07 15:10:58 +0100

    Data split (Train, Test) with label together

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 # Classification Library
 from sklearn import tree
 
+from sklearn.model_selection import train_test_split as split
+
 # This labels are species, genus and families.
 label_species = ['AdenomeraAndre', 'AdenomeraHylaedactylus', 'Ameeregatrivittata', 'HylaMinuta', 'HypsiboasCinerascens',
                  'HypsiboasCordobae',
@@ -42,14 +44,23 @@ def load_data():
 
 # Decision Tree
 def decision_tree(data, y1, y2, y3, p):
-    dt = tree.DecisionTreeClassifier()
+    dt = tree.DecisionTreeClassifier(criterion="entropy")
 
     # Training decision tree with data and label
     dt.fit(data, y1)
 
-    # dt.predict(p)
-    # t = dt.tree_
+    dt.predict(p)
+    t = dt.tree_
     # print(t.impurity)
+
+
+def k_split(x, y1, y2, y3):
+    # split x and y1 data, test size = ratio(default = 0.25), train_size = the remaining data
+    # random default
+    x_train, x_test, y_train, y_test = split(x, y1, test_size=0.2, random_state=123)
+    print(x_train.shape)
+
+
 
 if __name__ == "__main__":
     # Load data from csv file, X is data, Ys are labels
@@ -58,8 +69,9 @@ if __name__ == "__main__":
 
     # Sample Data checking
     predictData = [[1, 0.152936298, -0.105585903, 0.200721915, 0.317201062, 0.880763853, 0.100944641, -0.150062605,
-                   -0.171127632, 0.777776436, 0.188654146, 0.075621723, 0.156435925, 0.082245115, 0.135752042,
-                   0.024016645, 0.108351107, 0.077622521, 0.000567802, 0.057683975, 0.118680135, 0.014038446
-                   ]]
+                    -0.171127632, 0.777776436, 0.188654146, 0.075621723, 0.156435925, 0.082245115, 0.135752042,
+                    0.024016645, 0.108351107, 0.077622521, 0.000567802, 0.057683975, 0.118680135, 0.014038446
+                    ]]
 
     decision_tree(X, Y1, Y2, Y3, predictData)
+    k_split(X, Y1, Y2, Y3)
