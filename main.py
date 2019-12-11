7db09bb5f09bf75fd6c32a419a944e9d9b92eff3
MinsukKim <minsuk.kim.001@student.uni.lu>
2019-12-11 22:40:35 +0100

    Code refactoring

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import numpy as np
 # Classification Library
 from sklearn import tree
 
+# Data split function
 from sklearn.model_selection import train_test_split as split
 
 # This labels are species, genus and families.
 from decisiontree import decision_tree_new
+
+# Random Forest library
+from sklearn.ensemble import RandomForestClassifier
 
 label_species = ['AdenomeraAndre', 'AdenomeraHylaedactylus', 'Ameeregatrivittata', 'HylaMinuta', 'HypsiboasCinerascens',
                  'HypsiboasCordobae',
@@ -15,6 +19,9 @@ label_species = ['AdenomeraAndre', 'AdenomeraHylaedactylus', 'Ameeregatrivittata
 label_genus = ['Adenomera', 'Ameerega', 'Dendropsophus', 'Hypsiboas', 'Leptodactylus', 'Osteocephalus', 'Rhinella',
                'Scinax']
 label_families = ['Bufonidae', 'Dendrobatidae', 'Hylidae', 'Leptodactylidae']
+feature_names = ['MFCCs_ 1', 'MFCCs_ 2', 'MFCCs_ 3', 'MFCCs_ 4', 'MFCCs_ 5', 'MFCCs_ 6', 'MFCCs_ 7', 'MFCCs_ 8',
+                 'MFCCs_ 9', 'MFCCs_10', 'MFCCs_11', 'MFCCs_12', 'MFCCs_13', 'MFCCs_14', 'MFCCs_15', 'MFCCs_16',
+                 'MFCCs_17', 'MFCCs_18', 'MFCCs_19', 'MFCCs_20', 'MFCCs_21', 'MFCCs_22']
 
 
 # Data Loading function
@@ -41,41 +48,21 @@ def load_data():
                 label_f.append(label_families.index(row[-4]))
 
     return np.array(record).astype(float), np.array(label_s).astype(int), np.array(label_g).astype(int), np.array(
-        label_f).astype(int), record
+        label_f).astype(int)
 
 
-# Decision Tree
-def decision_tree(data, y1, y2, y3, p):
-    dt = tree.DecisionTreeClassifier(criterion="entropy")
-
-    # Training decision tree with data and label
-    dt.fit(data, y1)
-
-    dt.predict(p)
-    t = dt.tree_
-    # print(t.impurity)
-
-
-def k_split(x, y1, y2, y3):
+def data_split(x, y1, y2, y3):
     # split x and y1 data, test size = ratio(default = 0.25), train_size = the remaining data
-    # random default
-    x_train, x_test, y_train, y_test = split(x, y1, test_size=0.2, random_state=123)
-    print(x_train.shape)
+    # random default state 123
+    x_train, x_test, y_train, y_test = split(x, y1, test_size=0.4, random_state=123)
 
+    return x_train, x_test, y_train, y_test
 
 
 if __name__ == "__main__":
     # Load data from csv file, X is data, Ys are labels
-    X, Y1, Y2, Y3, record = load_data()
-    # print(X)
+    X, Y1, Y2, Y3 = load_data()
 
-    # Sample Data checking
-    predictData = [[1, 0.152936298, -0.105585903, 0.200721915, 0.317201062, 0.880763853, 0.100944641, -0.150062605,
-                    -0.171127632, 0.777776436, 0.188654146, 0.075621723, 0.156435925, 0.082245115, 0.135752042,
-                    0.024016645, 0.108351107, 0.077622521, 0.000567802, 0.057683975, 0.118680135, 0.014038446
-                    ]]
-
-    #decision_tree(X, Y1, Y2, Y3, predictData)
-    k_split(X, Y1, Y2, Y3)
-    decision_tree_new(record, X, Y1, Y2, Y3, predictData)
+    x_train, x_test, y_train, y_test = data_split(X, Y1, Y2, Y3)
+    decision_tree_new(x_train, x_test, y_train, y_test, feature_names)
 
