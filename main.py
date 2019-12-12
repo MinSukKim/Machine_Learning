6fdb09e183cbeef0a2a7eff3c4a1ae35488585b4
Peter191 <peter-hennen-191@hotmail.de>
2019-12-12 20:20:32 +0100

    relationCheck correction

--- main.py
+++ main.py
@@ -61,38 +61,6 @@ def data_split(x, y, i):
 
     return x_train, x_test, y_train, y_test
 
-
-def inconsistency(species, genus, families):
-    return species == label_species[1] \
-           or species == label_species[0] \
-           and genus == label_genus[0] \
-           and families == label_families[3] \
-           or species == label_species[6] \
-           and genus == label_genus[4] \
-           and families == label_families[3] \
-           or species == label_species[9] \
-           and genus == label_genus[7] \
-           and families == label_families[2] \
-           or species == label_species[4] \
-           and genus == label_genus[3] \
-           and families == label_families[2] \
-           or species == label_species[5] \
-           and genus == label_genus[3] \
-           and families == label_families[2] \
-           or species == label_species[7] \
-           and genus == label_genus[5] \
-           and families == label_families[2] \
-           or species == label_species[3] \
-           and genus == label_genus[2] \
-           and families == label_families[2] \
-           or species == label_species[8] \
-           and genus == label_genus[6] \
-           and families == label_families[0] \
-           or species == label_species[2] \
-           and genus == label_genus[1] \
-           and families == label_families[1]
-
-
 if __name__ == "__main__":
     # Load data from csv file, X is data, Ys are labels
     X, Y1, Y2, Y3 = load_data()
