50238f95d5f12a21627ae133c471cd4a7168cf02
MinsukKim <minsuk.kim.001@student.uni.lu>
2019-12-13 17:08:40 +0100

    RandomForest

--- randomForest.py
+++ randomForest.py
@@ -3,22 +3,46 @@ from sklearn.ensemble import RandomForestClassifier
 
 # metrics: accuracy calculation
 from sklearn import metrics
+from sklearn.metrics import confusion_matrix
 
-from sklearn import tree
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
 
 # Random Forest
-def random_Forest(x_train, x_test, y_train, y_test, feature_names, X, Y):
+def random_Forest(x_train, x_test, y_train, y_test, feature_names):
     # n_estimators = the number of trees
     # n_jobs = the number of core
-    rft = RandomForestClassifier(n_estimators=5, random_state=2)
+    rfts = RandomForestClassifier(n_estimators=10, random_state=0)
+    rftg = RandomForestClassifier(n_estimators=10, random_state=0)
+    rftf = RandomForestClassifier(n_estimators=10, random_state=0)
 
-    rft = rft.fit(x_train, y_train)
+    rfts = rfts.fit(x_train[0], y_train[0])
+    rftg = rftg.fit(x_train[1], y_train[1])
+    rftf = rftf.fit(x_train[2], y_train[2])
+
 
     # Predict the response for test dataset
-    y_pred = rft.predict(x_test)
-    # print(ys_pred)
+    y_preds = rfts.predict(x_test[0])
+    y_predg = rftg.predict(x_test[1])
+    y_predf = rftf.predict(x_test[2])
+
+    Y_pred=[y_preds, y_predg, y_predf]
+
+    i = 0
     # Model Accuracy, how often is the classifier correct?
-    print("Random Forest Accuracy: >>", metrics.accuracy_score(y_test, y_pred))
+    while i < 3:
+        print("Random Forest Accuracy: >>", metrics.accuracy_score(y_test[i], Y_pred[i]))
+        i+=1
 
-    # tree.plot_tree(rft)
+    # test = set(Y_pred)
+    # cm = pd.DataFrame(confusion_matrix(y_test, Y_pred), columns=test, index=test)
 
+    # cm['species'] = np.array([y_pred[i]] for i in y_pred)
+
+    # sns.pairplot(cm, hue='species')
+
+    # sns.heatmap(cm, annot=True)
+
+    return Y_pred
